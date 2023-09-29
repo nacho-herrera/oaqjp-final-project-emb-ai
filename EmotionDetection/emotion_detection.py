d6b56a46dfbf5934178e48811fd2e65d68425a34
Nacho Herrera <me@nachoherrera.com.ar>
2023-09-29 13:59:15 -0400

    final code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,21 +2,35 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    """
+    This functions create a request to the NLP Service and return the response.
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_format = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=headers, json=input_format)
     formatted_response = json.loads(response.text)
-    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    if response.status_code == 200:
+        emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    elif response.status_code  == 400:
+        emotion_scores = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+        }
+        dominant_emotion = None
+
     return_format = {
-      'anger': emotion_scores['anger'],
-      'disgust': emotion_scores['disgust'],
-      'fear': emotion_scores['fear'],
-      'joy': emotion_scores['joy'],
-      'sadness': emotion_scores['sadness'],
-      'dominant_emotion': dominant_emotion
-      }
+        'anger': emotion_scores['anger'],
+        'disgust': emotion_scores['disgust'],
+        'fear': emotion_scores['fear'],
+        'joy': emotion_scores['joy'],
+        'sadness': emotion_scores['sadness'],
+        'dominant_emotion': dominant_emotion
+        }
     return return_format
 
 
